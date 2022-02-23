a581470dd9c056f28756a3a8908a5977dd940762
Benjamin <bboksner@gmail.com>
2022-02-23 03:45:13 -0500

    commit tree ca89715a14d8acdebb3c96369d3e8cec18b58b8c

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+//const heart = document.getElementsByClassName('like-glyph');
+const heart = document.querySelectorAll('span.like-glyph');
+const errorBox = document.getElementById('modal')
+
 
 // Your JavaScript code goes here!
 
-
+  heart.forEach((heart)=>heart.addEventListener("click",()=>{
+    let clicked = false;
+    clicked = !clicked;
+    mimicServerCall()
+    .then(response=> {
+      console.log(response);
+      heart.classList.toggle("activated-heart")
+      if (clicked){
+        heart.innerHTML = FULL_HEART;
+      }
+      else{
+        heart.innerHTML = EMPTY_HEART;
+      }
+    })
+    .catch(error =>{ 
+      heart.innerHTML = EMPTY_HEART;
+        heart.classList.remove("activated-heart")
+      console.log(`***ERROR!*** ${error} ***ERROR!***`)
+      errorBox.classList.remove('hidden');
+      setTimeout(() => {
+        errorBox.classList.add('hidden');
+      }, 3000);
+      
+    
+    })
+})) 
 
 
 //------------------------------------------------------------------------------
